e635d9445db5c4cd9d4bf6b354d7ce0c0165cd73
Addison Young <addisonyoung0902@gmail.com>
2025-04-08 19:55:48 -0400

    figure out mouseover and click events

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let mode = "Click";
 let amount = 0;
 const BOARD_WH = 500;
-createBoard(10);
+let clickButton = document.querySelector('.click');
+let deleteButton = document.querySelector('.delete');
+createBoard(8);
 
 
 function createBoard(amount)
@@ -15,3 +18,51 @@ function createBoard(amount)
   }
   document.querySelector(".board").innerHTML = html;
 }
+
+configClickEvents();
+
+function configClickEvents()
+{
+  let pixels = document.querySelectorAll('.square');
+  for(let i = 0; i < pixels.length; i++)
+  {
+    let pixel = pixels[i];
+    pixel.addEventListener('mouseover', () => {
+      decideAction(pixel);
+     // console.log('hi');
+
+    });
+  }
+}
+
+function decideAction(pixel)
+{
+  let selectedColor = document.querySelector('.color').value;
+  if(mode === "Click")
+  {
+    pixel.style.backgroundColor = selectedColor;
+  }
+
+  if(mode == 'Delete')
+  {
+    pixel.style.backgroundColor = "white";
+  }
+  
+}
+
+
+
+clickButton.addEventListener('click', () => {
+  mode = "Click";
+  document.querySelector('.click').classList.remove('selected');
+  document.querySelector('.delete').classList.remove('selected');
+  clickButton.classList.add('selected');
+});
+
+deleteButton.addEventListener('click', () => {
+  mode = "Delete";
+  document.querySelector('.click').classList.remove('selected');
+  document.querySelector('.delete').classList.remove('selected');
+ 
+  deleteButton.classList.add('selected');
+});
